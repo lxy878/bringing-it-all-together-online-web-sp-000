b57c7020d0b0ffec69aef83a1a1e7745c5602fa9
Xiaoyi Liu <lxy878@gmail.com>
2020-03-30 14:51:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,4 +62,11 @@ class Dog
   def self.find_or_create_by(name:, breed:)
 
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
+    SQL
+
+  end
 end
