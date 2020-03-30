5936e0e469e98c9e40f1d86ac1214ecfcca8736d
Xiaoyi Liu <lxy878@gmail.com>
2020-03-30 14:47:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,7 @@ class Dog
     sql = <<-SQL
       SELECT * FROM dogs WHERE id = ?
     SQL
-    row = DB[:conn].execute(sql, id)
+    row = DB[:conn].execute(sql, id).first
     binding.pry
   end
 
